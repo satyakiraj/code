1540c9fc6e958f9b3b24af02f8244e2b2b79ba25
satyakiraj <satyakithecoder@gmail.com>
2024-08-11 21:38:20 +0530

    Automatically adding new files/folders in /home/satyakithecoder/code

---
+++ fullstack development/google-drive-clone/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/{svelte,js,ts,css}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
